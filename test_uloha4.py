7bc253e83bf4055cb61daf6264834a9def1b3921
martin.haluska <martin.haluska@KSC1-LHP-K10745>
2023-07-19 15:33:29 +0200

    update

---
+++ test_uloha4.py
@@ -0,0 +1,23 @@
+uloha = "- vstup z klavecnice nejaky retazec znakov a nasledne zamenit poradie znakov v danom retazci (napr Peto -> oteP)"
+
+text = input("Zadaj text:\n")
+
+x = (text[::-1])
+print(x)
+
+
+####
+
+#text_pocet = len(text)
+#x - pocet_znakov
+#print(text_pocet)
+#print(text[-x:0])
+
+
+###########################3
+#while (text[0]) is
+
+##########
+#if x == [0] in text:
+
+#print(text[x:y])
