7557411159d5799df3cf3cdde23fa4d2d6bd1c1d
martin.haluska <martin.haluska@KSC1-LHP-K10745>
2023-09-29 09:16:43 +0200

    update

---
+++ Excel tests/test/test 1].py
@@ -0,0 +1,40 @@
+# tri premennte 1. integer, 2 string, 3 pole
+#
+# a = int(1)
+# print(a)
+b = "test"
+# print(b)
+# c = ["alpha", "beta", "gamma"]
+# print(c)
+# d = [1,2,3]
+# print(d)
+# strene pismena vypisat 2 alebo 1 podla poctu
+
+c = len(b)
+print(c)
+
+#e = b % 2
+#print(e)
+#print(b[x:(y+1)])
+#else print([z])
+
+
+
+
+#zisti ci je cislo parne alebo neparne
+
+# a = int(input("Zadaj cislo\n"))
+# print(a)
+#
+# if (a / 2) == 0:
+#         print("parne")
+# else:
+#         print("neparne")
+
+# zadefinovany text
+
+
+
+
+
+
